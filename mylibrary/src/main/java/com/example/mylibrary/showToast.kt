d43a5707efe3cdf204ee665c025003a04ccf1d3b
Apoorv Singh <apoorva.singh@appinventiv.com>
2024-01-29 12:42:40 +0530

    Added Toast

--- mylibrary/src/main/java/com/example/mylibrary/showToast.kt
+++ mylibrary/src/main/java/com/example/mylibrary/showToast.kt
@@ -3,15 +3,16 @@ package com.example.mylibrary
 import android.app.Activity
 import android.content.Context
 import android.widget.Toast
+import com.example.mylibrary.ShowToast.Companion.ShowToast
 
 
 class ShowToast {
 
+    fun Context.ShowToast(msz: String) {
+        Toast.makeText(this, msz, Toast.LENGTH_LONG).show()
+    }
     companion object {
-        @JvmStatic
-        fun Context.ShowToast(msz: String) {
-            Toast.makeText(this, msz, Toast.LENGTH_LONG).show()
-        }
+
 
         @JvmStatic
         fun Activity.ShowToast(msz: String) {
